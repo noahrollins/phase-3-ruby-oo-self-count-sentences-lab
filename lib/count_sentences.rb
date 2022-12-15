33dbb68d840996ccc2a0a0188dacc0860246fbc9
Noah Rollins <noahjrollins@gmail.com>
2022-12-15 10:26:09 -0600

    commit tree a122a21ff9c802a5e51c52d20820dfcfacb5b355

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,26 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence?()
+    self.end_with?(".")
   end
 
-  def question?
-
+  def question?()
+    self.end_with?("?")
   end
 
-  def exclamation?
-
+  def exclamation?()
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = 0
+    punctuation = [".","?","!"]
+    self.chars.each.with_index do |chars, index|
+      if punctuation.include?(chars) && !punctuation.include?(self[index + 1])
+        sentences += 1
+      end
+    end
+    sentences
   end
 end
